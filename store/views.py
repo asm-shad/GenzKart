32d924dec3dff5c3dea7c2636280c3e3ee511cef
ASM Shad <asmshad@gmail.com>
2026-06-28 11:16:10 +0600

    Cart and Cart Icon functionality implemented

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
+from carts.models import CartItem
+from carts.views import _cart_id
+from django.http import HttpResponse
 
 # Create your views here.
 def store(request, category_slug=None):
@@ -26,7 +29,9 @@ def store(request, category_slug=None):
 def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        # in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
+        # return HttpResponse(in_cart)
+        # exit()
     except Exception as e:
         raise e
 
@@ -46,7 +51,7 @@ def product_detail(request, category_slug, product_slug):
 
     context = {
         'single_product': single_product,
-        # 'in_cart'       : in_cart,
+        'in_cart'       : in_cart,
         # 'orderproduct': orderproduct,
         # 'reviews': reviews,
         # 'product_gallery': product_gallery,
