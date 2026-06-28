3f89bffb8ad4ca4ac0c512e9a00be3f7b7531280
ASM Shad <asmshad@gmail.com>
2026-06-28 14:19:45 +0600

    Pagination and search functionality implemented

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
@@ -52,23 +53,28 @@ def remove_cart_item(request, product_id):
     cart_item.delete()
     return redirect('cart')
 
-def cart(request):
+def cart(request, total=0, quantity=0, cart_items=None):
     try:
-        cart = Cart.objects.get(cart_id= _cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        tax = 0
+        grand_total = 0
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except:
-        pass
+    except ObjectDoesNotExist:
+        pass #just ignore
 
     context = {
         'total': total,
         'quantity': quantity,
         'cart_items': cart_items,
-        'tax': tax,
+        'tax'       : tax,
         'grand_total': grand_total,
     }
-    return render(request, "store/cart.html", context)
+    return render(request, 'store/cart.html', context)
