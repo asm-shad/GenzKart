12786b7287b3b11f83f62ae60a52ab7cdd009664
ASM Shad <asmshad@gmail.com>
2026-06-30 11:17:27 +0600

    Variation in Cart, Grouping Cart Item Variations

--- carts/models.py
+++ carts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from store.models import Product
+from store.models import Product, Variation
 from accounts.models import Account
+
 
 # Create your models here.
 
@@ -10,11 +11,12 @@ class Cart(models.Model):
 
     def __str__(self):
         return self.cart_id
-    
+
+
 class CartItem(models.Model):
     user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # variations = models.ManyToManyField(Variation, blank=True)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart    = models.ForeignKey(Cart, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
